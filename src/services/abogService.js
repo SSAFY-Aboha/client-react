2198f50161c2f37e406c4641fec6b60d128a8f64
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2024-12-21 00:36:04 +0900

    refactor: api호출 중 react query 사용 방법 변경

--- src/services/abogService.js
+++ src/services/abogService.js
@@ -2,20 +2,25 @@ import apiClient from '@/services/http-common';
 import { useQuery, useMutation } from '@tanstack/react-query';
 
 // 아보그 조회
-const useGetAbog = () => {
-  return useQuery(['getAbog'], () => apiClient.get('/abogs'));
+export const useGetAbog = () => {
+  return useQuery({
+    queryKey: ['getAbog'],
+    queryFn: () => apiClient.get('/abogs'),
+  });
 };
 
 // 아보그 상세 조회
-const useGetAbogDetail = (abogId) => {
-  return useQuery(['getAbogDetail', abogId], () =>
-    apiClient.get(`/abogs/${abogId}`),
-  );
+export const useGetAbogDetail = (abogId) => {
+  return useQuery({
+    queryKey: ['getAbogDetail', abogId],
+    queryFn: () => apiClient.get(`/abogs/${abogId}`),
+  });
 };
 
 // 아보그 추가
-const useCreateAbog = () => {
-  return useMutation((abog) => apiClient.post('/abogs', abog), {
+export const useCreateAbog = () => {
+  return useMutation({
+    mutationFn: (abog) => apiClient.post('/abogs', abog),
     headers: {
       'Content-Type': 'multipart/form-data',
     },
@@ -23,47 +28,48 @@ const useCreateAbog = () => {
 };
 
 // 아보그 수정
-const useUpdateAbog = () => {
-  return useMutation((abog) => apiClient.put('/abogs', abog));
+export const useUpdateAbog = () => {
+  return useMutation({
+    mutationFn: (abog) => apiClient.put('/abogs', abog),
+  });
 };
 
 // 아보그 삭제
-const useDeleteAbog = () => {
-  return useMutation((abog) => apiClient.delete(`/abogs/${abog.id}`));
+export const useDeleteAbog = () => {
+  return useMutation({
+    mutationFn: (abog) => apiClient.delete(`/abogs/${abog.id}`),
+  });
 };
 
 // 아보그 좋아요 토글
-const useToggleAbogLike = () => {
-  return useMutation((abogId) => apiClient.post(`/abogs/${abogId}/like`));
+export const useToggleAbogLike = () => {
+  return useMutation({
+    mutationFn: (abogId) => apiClient.post(`/abogs/${abogId}/like`),
+  });
 };
 
 // 아보그 댓글 조회
-const useGetAbogComments = (abogId) => {
-  return useQuery(['getAbogComments', abogId], () =>
-    apiClient.get(`/abogs/${abogId}/comments`),
-  );
+export const useGetAbogComments = (abogId) => {
+  return useQuery({
+    queryKey: ['getAbogComments', abogId],
+    queryFn: () => apiClient.get(`/abogs/${abogId}/comments`),
+  });
 };
 
 // 아보그 댓글 추가
-const useAddAbogComment = () => {
-  return useMutation((abogId, content) =>
-    apiClient.post('/comments', { abogId, content }),
-  );
+export const useAddAbogComment = () => {
+  return useMutation({
+    mutationFn: (params) =>
+      apiClient.post('/comments', {
+        abogId: params.abogId,
+        content: params.content,
+      }),
+  });
 };
 
 // 아보그 댓글 삭제
-const useDeleteAbogComment = () => {
-  return useMutation((commentId) => apiClient.delete(`/comments/${commentId}`));
-};
-
-export default {
-  useGetAbog,
-  useGetAbogDetail,
-  useCreateAbog,
-  useUpdateAbog,
-  useDeleteAbog,
-  useToggleAbogLike,
-  useGetAbogComments,
-  useAddAbogComment,
-  useDeleteAbogComment,
+export const useDeleteAbogComment = () => {
+  return useMutation({
+    mutationFn: (commentId) => apiClient.delete(`/comments/${commentId}`),
+  });
 };
